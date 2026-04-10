50e07f28df6721d28822860e8171dddd21a555e0
Ryan Baker <rbaker@xtier.com>
2026-04-10 00:58:57 -0400

    Add rm -rf to checkout.sh

--- tasks/task-simple/checkout.sh
+++ tasks/task-simple/checkout.sh
@@ -8,6 +8,7 @@ REPO_URL="https://github.com/rybkr/textstats"
 BASE_BRANCH="task-simple"
 TASK_DIR="repo"
 
+rm -rf "$TASK_DIR"
 git clone "$REPO_URL" "$TASK_DIR"
 cd "$TASK_DIR"
 git checkout "$BASE_BRANCH"
